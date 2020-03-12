299504f03caec372f142d60efad8619df5612723
airenas <airenass@gmail.com>
2020-03-12 14:04:00 +0200

    Update path.sh

--- egs/lab/tts1/path.sh
+++ egs/lab/tts1/path.sh
@@ -1,5 +1,5 @@
 MAIN_ROOT=$PWD/../../..
-KALDI_ROOT=/home/airenas/projects/kaldi_test/kaldi
+KALDI_ROOT=$MAIN_ROOT/tools/kaldi
 
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/sctk/bin:$PWD:$PATH
