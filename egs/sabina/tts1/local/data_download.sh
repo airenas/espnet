8175b29843733ce5c81dd1274c922a82c49bbbc3
airenas <airenass@gmail.com>
2020-03-13 18:48:18 +0200

    Improve scripts

--- egs/sabina/tts1/local/data_download.sh
+++ egs/sabina/tts1/local/data_download.sh
@@ -14,7 +14,7 @@ fi
 set -euo pipefail
 
 cwd=$(pwd)
-if [ ! -e "${download_dir}/sapina.v01" ]; then
+if [ ! -e "${download_dir}/sabina.v01" ]; then
     mkdir -p "${download_dir}"
     cd "${download_dir}"
     #wget http://data.keithito.com/data/speech/LJSpeech-1.1.tar.bz2
