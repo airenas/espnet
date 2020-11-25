a77e6d99f4e460d8c2d1116f93452278eaa97c63
Airenas Vaičiūnas <airenass@gmail.com>
2020-11-25 09:52:48 +0200

    Updating scripts

--- egs2/sabina/tts1/local/data_download.sh
+++ egs2/sabina/tts1/local/data_download.sh
@@ -1,1 +1,27 @@
-../../../../egs/ljspeech/tts1/local/data_download.sh
+#!/bin/bash
+
+# Copyright 2019 Tomoki Hayashi
+#  Apache 2.0  (http://www.apache.org/licenses/LICENSE-2.0)
+
+download_dir=$1
+
+# check arguments
+if [ $# != 1 ]; then
+    echo "Usage: $0 <download_dir>"
+    exit 1
+fi
+
+set -euo pipefail
+
+cwd=$(pwd)
+if [ ! -e "${download_dir}/${corpus}" ]; then
+    mkdir -p "${download_dir}"
+    cp ${corpus_file} ${download_dir}/
+    cd "${download_dir}" 
+    tar -vxf ./*.tar.gz
+    rm ./*.tar.gz
+    cd "${cwd}"
+    echo "successfully prepared data."
+else
+    echo "already exists. skipped."
+fi
