966b1bf55d4e207df6c4b98363494966127f5025
airenas <airenass@gmail.com>
2021-06-25 15:04:11 +0300

    Add 'vytautas' corpus

---
+++ egs2/vytautas/tts1/local/data.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+log() {
+    local fname=${BASH_SOURCE[1]##*/}
+    echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
+}
+SECONDS=0
+
+stage=-1
+stop_stage=2
+
+log "$0 $*"
+. utils/parse_options.sh
+
+if [ $# -ne 0 ]; then
+    log "Error: No positional arguments are required."
+    exit 2
+fi
+
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+
+db_root=${work_dir}/downloads
+
+train_set=tr_no_dev
+train_dev=dev
+eval_set=eval1
+
+if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
+    log "stage -1: Data Download"
+    local/data_download.sh "${db_root}"
+fi
+
+ct=3
+if [ "${trans_type}" = phn ]; then
+    ct=4
+fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    log "stage 0: Data Preparation"
+    # set filenames
+    scp=${work_dir}/data/train/wav.scp
+    utt2spk=${work_dir}/data/train/utt2spk
+    spk2utt=${work_dir}/data/train/spk2utt
+    text=${work_dir}/data/train/text
+
+    # check file existence
+    [ ! -e ${work_dir}/data/train ] && mkdir -p ${work_dir}/data/train
+    [ -e ${scp} ] && rm ${scp}
+    [ -e ${utt2spk} ] && rm ${utt2spk}
+    [ -e ${spk2utt} ] && rm ${spk2utt}
+    [ -e ${text} ] && rm ${text}
+
+    # make scp, utt2spk, and spk2utt
+    find ${db_root}/${corpus} -follow -name "*.wav" | sort | while read -r filename;do
+        id=$(basename ${filename} | sed -e "s/\.[^\.]*$//g")
+        echo "${id} ${filename}" >> ${scp}
+        echo "${id} ${speaker}" >> ${utt2spk}
+    done
+    utils/utt2spk_to_spk2utt.pl ${utt2spk} > ${spk2utt}
+
+    # make text usign the original text
+    # cleaning and phoneme conversion are performed on-the-fly during the training
+    paste -d " " \
+        <(cut -d "|" -f 1 < ${db_root}/${corpus}/metadata.csv) \
+        <(cut -d "|" -f ${ct} < ${db_root}/${corpus}/metadata.csv) \
+        > ${text}
+
+    utils/validate_data_dir.sh --no-feats ${work_dir}/data/train
+fi
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    log "stage 2: utils/subset_data_dir.sg"
+    # make evaluation and devlopment sets
+    _ta=$(( ${test_count} * 2 ))
+    utils/subset_data_dir.sh --last ${work_dir}/data/train ${_ta} ${work_dir}/data/deveval
+    utils/subset_data_dir.sh --last ${work_dir}/data/deveval ${test_count} ${work_dir}/data/${eval_set}
+    utils/subset_data_dir.sh --first ${work_dir}/data/deveval ${test_count} ${work_dir}/data/${train_dev}
+    _n=$(( $(wc -l < ${work_dir}/data/train/wav.scp) - ${_ta} ))
+    echo "Test files: ${test_count}, Dev: ${test_count}, Train: ${_n}"
+    utils/subset_data_dir.sh --first ${work_dir}/data/train ${_n} ${work_dir}/data/${train_set}
+fi
+
+log "Successfully finished. [elapsed=${SECONDS}s]"
