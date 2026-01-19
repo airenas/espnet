e981d745aab8f08a32e7b588d66ad7b54912de86
Airenas Vaiciunas <airenass@gmail.com>
2026-01-19 18:42:56 +0200

    fix: add f0 ranges

--- egs2/vdu/tts1/run.sh
+++ egs2/vdu/tts1/run.sh
@@ -8,6 +8,10 @@ set -o pipefail
 fs=22050
 n_fft=1024
 n_shift=256
+f0min=65
+f0max=330
+cleaner=lower
+token_type=char
 
 opts=
 if [ "${fs}" -eq 22050 ]; then
@@ -33,8 +37,8 @@ g2p=g2p_en_no_space # Include no word separator
     --fs "${fs}" \
     --n_fft "${n_fft}" \
     --n_shift "${n_shift}" \
-    --token_type char \
-    --cleaner lower \
+    --token_type "${token_type}" \
+    --cleaner "${cleaner}" \
     --g2p "${g2p}" \
     --train_config "${train_config}" \
     --inference_config "${inference_config}" \
@@ -42,4 +46,5 @@ g2p=g2p_en_no_space # Include no word separator
     --valid_set "${valid_set}" \
     --test_sets "${test_sets}" \
     --srctexts "data/${train_set}/text" \
+    --f0min ${f0min} --f0max ${f0max} \
     ${opts} "$@"
